02e1f180f73b329fe521087dae7e80a7806f86ff
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2025-04-26 15:56:18 +0530

    Initialize Firebase in the main application and set up the auth module

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:smart_shopping_assistant/screens/welcomeScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
