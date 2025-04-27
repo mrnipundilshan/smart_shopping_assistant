6665a9b4f2004833f5135cca0235ad68503b0d55
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2025-04-27 12:56:14 +0530

    Refactor textfield widget to support password visibility toggle and update WelcomeScreen to use new functionality

--- lib/services/reuse/textfield.dart
+++ lib/services/reuse/textfield.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 
-class textfield extends StatelessWidget {
+class textfield extends StatefulWidget {
   final TextEditingController reusetexteditincontroller;
   final String text;
   final Icon icon;
+  final bool password;
 
   const textfield({
     super.key,
     required this.reusetexteditincontroller,
     required this.text,
     required this.icon,
+    required this.password,
   });
+
+  @override
+  State<textfield> createState() => _textfieldState();
+}
+
+class _textfieldState extends State<textfield> {
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
@@ -19,14 +28,29 @@ class textfield extends StatelessWidget {
     return SizedBox(
       width: width * 0.8,
       child: TextField(
-        controller: reusetexteditincontroller,
+        controller: widget.reusetexteditincontroller,
+        obscureText: widget.password ? _obscureText : false,
         decoration: InputDecoration(
+          prefixIcon: widget.icon,
           filled: true,
           fillColor: Colors.green[50],
           iconColor: Color(0xFF062920),
           hintStyle: TextStyle(color: Color(0xFF062920)),
-          hintText: text,
-          suffixIcon: icon,
+          hintText: widget.text,
+          suffixIcon:
+              widget.password
+                  ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                    icon: Icon(
+                      _obscureText ? Icons.visibility_off : Icons.visibility,
+                      color: const Color(0xFF062920),
+                    ),
+                  )
+                  : null,
           contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
