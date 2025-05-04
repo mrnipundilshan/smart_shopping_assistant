5fc1b5e4e1f8033bdd051a3f912a3d3db0e57c57
Nipun Dilshan <nipundilshan991@gmail.com>
2025-05-04 08:09:54 +0530

    Add sign-out functionality to homepage; implement logout method and update UI accordingly

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:smart_shopping_assistant/auth/auth.dart';
 import 'package:smart_shopping_assistant/services/background/bg.dart';
+import 'package:smart_shopping_assistant/services/reuse/buttons.dart';
 
 class homepage extends StatefulWidget {
   const homepage({super.key});
@@ -11,6 +13,16 @@ class homepage extends StatefulWidget {
 class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
-    return Background(column: Column(children: [Text("Logged")]));
+    return Background(
+      column: Column(
+        children: [functionbutton(text: "Sign Out", function: _logout)],
+      ),
+    );
+  }
+
+  Future<void> _logout() async {
+    AuthService().signout().then((value) {
+      Navigator.pushReplacementNamed(context, '/first');
+    });
   }
 }
